1a359c3d799ede6c35fa29badd1e6a0863b84672
Saurabh Gupta <saurabhrf161989@gmail.com>
2021-12-15 17:20:33 +0530

    saurabh changes

--- Manika/src/Programs/HelloWorld.java
+++ Manika/src/Programs/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello world master");
+		System.out.println("hello world");
 	}
 
 }
