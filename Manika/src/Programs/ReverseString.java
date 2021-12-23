a2ecd790212f1146495ca00913d5e23f202dc591
Saurabh Gupta <saurabhrf161989@gmail.com>
2021-12-23 18:25:36 +0530

    changes added

--- Manika/src/Programs/ReverseString.java
+++ Manika/src/Programs/ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String s= "saurabh";
+String s= "saurabh12";
 String rev = "";
 for(int i = s.length()-1; i>=0; i--) {
 	char c = s.charAt(i);
